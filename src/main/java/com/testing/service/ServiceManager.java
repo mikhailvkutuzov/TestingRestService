c231436a89068aade0be9ed040fcf0e550b77879
mikhail.kutuzov <mikhail.kutuzov.geargames@gmail.com>
2017-07-07 21:49:26 +0300

    put [urlToBeTested] into server arguments

--- src/main/java/com/testing/service/ServiceManager.java
+++ src/main/java/com/testing/service/ServiceManager.java
@@ -7,7 +7,9 @@ import test.smoketest.utils.*;
 
 import java.io.File;
 import java.net.URI;
+import java.net.URL;
 
+@SuppressWarnings("all")
 public class ServiceManager {
     public static final String BASE_URI = "http://localhost:8080/testing/";
 
@@ -39,27 +41,46 @@ public class ServiceManager {
     public static void copyDocXResources() throws Exception {
         FileFromResource copier = new SingleFileByPath();
         File outDirectory = new File("target/classes/resources");
-        if(!outDirectory.exists()) {
+        if (!outDirectory.exists()) {
             outDirectory.mkdirs();
         }
-        for(String resource : ClassesFromPackage.getResourcesByExtent("resources",
+        for (String resource : ClassesFromPackage.getResourcesByExtent("resources",
                 s -> s.endsWith("docx")
                         || s.endsWith(".doc")
                         || s.endsWith(".odt")
                         || s.endsWith(".txt")
                         || s.endsWith(".rtf")
-        )){
+        )) {
             String flatName = resource.split("resources")[1].substring(1);
-            File file = new File("target/classes/resources/"+flatName);
+            File file = new File("target/classes/resources/" + flatName);
             file.deleteOnExit();
-            if(!file.exists()) {
+            if (!file.exists()) {
                 copier.create(resource, file);
             }
         }
     }
 
 
+    /**
+     * This service starts if and only if [urlToBeTested] is handed on as an argument.
+     * @param args urlToBeTested=http://<something>
+     * @throws Exception if no valid arguments are passed
+     */
     public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            throw new IllegalAccessException("Only one argument should be present");
+        }
+        String[] urlToBeTested = args[0].split("=");
+        if (urlToBeTested.length != 2) {
+            throw new IllegalAccessException();
+        } else {
+            if (!urlToBeTested[0].equals("urlToBeTested")) {
+                throw new IllegalAccessException("An argument name is not ");
+            }
+        }
+        new URL(urlToBeTested[1]);
+        System.setProperty("urlToBeTested",  urlToBeTested[1]);
+
         final HttpServer server = start("com.testing.service");
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
